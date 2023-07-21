afe976e2b7a5efeb5d2251026098446247d48d53
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-07-21 11:48:13 +0200

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import "./global.css";
+import s from "./style.module.css";
+
 export function App() {
     return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
+            <div className={s.main_container}>
+                <div className={s.header}>
+                    <div className="row">
+                        <div className="col-4">
+                            <div>Logo</div>
+                            <div>Subtitle</div>
+                        </div>
+                        <div className="col-md-12 col-lg-4">
+                            <input style={{width : "100%"}} type="text"/>
+                        </div>
+                    </div>
+                </div>
+                <div className={s.tv_show_detail}>Detail</div>
+                <div className={s.recommendations}>Recommendations</div>
+            </div>
     );
 }
